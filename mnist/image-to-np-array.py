a9cdac8da47fe8fc2864d44cb8e45ea8fccfda54
Fu, Biye <biye.fu@outlook.com>
2019-11-05 23:39:55 +0800

    add test

---
+++ mnist/image-to-np-array.py
@@ -0,0 +1,17 @@
+from PIL import Image
+import numpy as np,os
+import tensorflow as tf
+model = tf.keras.models.load_model('D:\\workspace\\mnist\\mnist.h5')
+for file in os.listdir('img'):
+    if not file.startswith('origin'):
+        continue
+    if not file.endswith('.png'):
+        continue
+    im = Image.open('img/' + file)
+    im.show()
+    imArray = np.array(im)
+    predictions = model.predict([[imArray]])
+    print("Prediction of image:" + file)
+    print(np.argmax(predictions[0]))
+    print('\n')
+print('Done!')
